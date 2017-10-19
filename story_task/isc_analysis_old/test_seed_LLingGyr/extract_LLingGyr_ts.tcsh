3fbd6fa25e5c00a45b4855ed0ef72069354d36d4
esfinn <esfinn@gmail.com>
2017-10-19 03:00:04 -0400

    Auto-Update Thu Oct 19 03:00:04 EDT 2017

--- story_task/isc_analysis_old/test_seed_LLingGyr/extract_LLingGyr_ts.tcsh
+++
@@ -1,5 +0,0 @@
-foreach i (tb2994  tb3240  tb3512  tb3592  tb3626  tb3744  tb3784  tb3846  tb3920  tb3964  tb4450  tb4572 tb3132  tb3279  tb3552  tb3602  tb3646  tb3757  tb3810  tb3858  tb3929  tb3977  tb4547)
-
-3dmaskave -mask LLingGyr_p0093_clust433+tlrc.HEAD /data/finnes/story_task/${i}/${i}_pass1.results/blur4_story_audioTRs_errts.${i}_pass1.fanaticor+tlrc.HEAD -quiet > LLingGyr_p0093_clust433_${i}_story_audioTRs.txt
-
-end
--- story_task/preprocessing/do_aproc_StoryVolume_noFS.sh
+++ story_task/preprocessing/do_aproc_StoryVolume_noFS.sh
@@ -5,54 +5,42 @@ module load afni
 # Set top level directory structure
 topdir=/data/finnes/story_task/
 task=StoryAll
-fsroot=$topdir/preprocessing/freesurfer/
 
 for subj in $@
 do
 
-# Input directory: FreeSurfer + @SUMA_MakeSpec_FS results
-fsindir=$fsroot/$subj/SUMA/
-
 # Output directory: name for output
 outdir=$topdir/$subj
 
-# Input data: FreeSurfer results - anatomy, ventricle and WM maps
-fsanat=${subj}_SurfVol.nii
-fsvent=FSmask_vent.nii
-fswm=FSmask_WM.nii
-
 # run afni_proc.py to create a single subject processing script
 afni_proc.py -subj_id $subj                                                 \
-        -script /$topdir/preprocessing/procVol.$subj.${task}TEST -scr_overwrite   \
-	-out_dir /$topdir/$subj/$subj.$task.volResultsTEST      		    \
+        -script /$topdir/preprocessing/procVol.$subj.${task} -scr_overwrite   \
+	-out_dir /$topdir/$subj/$subj.$task.volResults      		    \
         -blocks despike align tlrc volreg blur mask regress 		    \
-        -copy_anat /$fsindir/$fsanat    				    \
-    		-anat_follower_ROI aaseg  anat $fsindir/aparc.a2009s+aseg_rank.nii  \
-    		-anat_follower_ROI aeseg  epi  $fsindir/aparc.a2009s+aseg_rank.nii  \
-    		-anat_follower_ROI FSvent epi  $fsindir/$fsvent                     \
-    		-anat_follower_ROI FSWMe  epi  $fsindir/$fswm                       \
-    		-anat_follower_erode FSvent FSWMe                                   \
+        -copy_anat /$topdir/$subj/${subj}_T1_shft.nii.gz  	                                            \
         -dsets \
 		/$topdir/$subj/${subj}_func_Story1_crop.nii.gz            \
 		/$topdir/$subj/${subj}_func_Story2_crop.nii.gz            \
 		/$topdir/$subj/${subj}_func_Story3_crop.nii.gz            \
         -tcat_remove_first_trs 0                                            \
-	-align_opts_aea -ginormous_move 					    \
+	-align_opts_aea -ginormous_move 			            \
 	-tlrc_base TT_N27+tlrc                                              \
-   	-tlrc_NL_warp                                                       \
+   	-tlrc_NL_warp                                         	            \
     	-volreg_align_to MIN_OUTLIER                                        \
     	-volreg_align_e2a                                                   \
     	-volreg_tlrc_warp                                                   \
 	-blur_size 5						 	    \
-    	-regress_ROI_PC FSvent 3                                            \
-    	-regress_make_corr_vols aeseg FSvent                                \
+        -mask_segment_anat yes                                     	    \
+        -mask_import Tvent /data/finnes/story_task/template_ventricle_2.0+tlrc \
+        -mask_intersect Svent CSFe Tvent  				    \
+    	-regress_ROI_PC Svent 3                                            \
+    	-regress_make_corr_vols WMe Svent                                \
     	-regress_anaticor_fast                                              \
-    	-regress_anaticor_label FSWMe                                       \
     	-regress_censor_motion 0.3                                          \
     	-regress_censor_outliers 0.1                                        \
     	-regress_apply_mot_types demean deriv                               \
     	-regress_est_blur_epits                                             \
     	-regress_est_blur_errts                                             \
-    	-regress_run_clustsim no					    \
+    	-regress_run_clustsim yes					    \
 	-bash -execute
 done
