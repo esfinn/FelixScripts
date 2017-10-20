ee7c25e856e3d5b12f5f8e426ce9fefeb772be42
esfinn <esfinn@gmail.com>
2017-10-20 03:00:05 -0400

    Auto-Update Fri Oct 20 03:00:04 EDT 2017

---
+++ story_task/isc_analysis/batch_compute_pairwiseISC.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+module load afni
+
+topdir=/data/finnes/story_task/
+
+# tb3552 excluded
+subjlist=(tb2994 tb3132 tb3240 tb3279 tb3512 tb3592 tb3602 tb3626 tb3646 tb3744 tb3757 tb3784 tb3810 tb3846 tb3858 tb3920 tb3929 tb3964 tb3977 tb4450 tb4547 tb4572)
+
+subjlist_length=${#subjlist[@]}
+
+for i in `seq 1 $subjlist_length`
+do
+
+s1=${subjlist[$i-1]}
+
+for j in `seq 1 $subjlist_length`
+do
+
+s2=${subjlist[$j-1]}
+
+if (($i<$j))
+then
+
+echo Doing ${s1} and ${s2}
+
+filename=$topdir/isc_analysis/pairwise_iscMaps/${s1}_${s2}_StoryAll.volResults_errts.fanaticor+tlrc.HEAD
+
+echo ${s1} ${s2} $filename >> data_n22.txt
+
+if [ -e $filename ]
+then
+
+echo ${s1} and ${s2} already exists
+
+else
+echo 3dTcorrelate -prefix $filename -polort -1 -automask $topdir/$s1/${s1}.StoryAll.volResults/errts.${s1}.fanaticor+tlrc.HEAD $topdir/$s2/${s2}.StoryAll.volResults/errts.${s2}.fanaticor+tlrc.HEAD >> Tcorrelate_pairs.txt
+
+fi
+fi
+
+done
+done
